0510c10fe6ce5b2e0118e59f2f3a682537afe5e8
Ruslan Moiseyenko <ruscom5@gmail.com>
2024-05-15 21:24:59 +0300

    added splash screens

--- android/app/src/main/java/com/news_rn_cli/MainActivity.kt
+++ android/app/src/main/java/com/news_rn_cli/MainActivity.kt
@@ -5,6 +5,9 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
+import android.os.Bundle
+import com.zoontek.rnbootsplash.RNBootSplash
+
 class MainActivity : ReactActivity() {
 
   /**
@@ -19,4 +22,10 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    RNBootSplash.init(this, R.style.BootTheme) 
+    super.onCreate(savedInstanceState) 
+  }
+
 }
